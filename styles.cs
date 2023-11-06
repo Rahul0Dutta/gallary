82d30706ef65a9ed3d02963ea172b4ca53e8716d
Rahul0Dutta <duttarahul0r0@gmail.com>
2023-11-06 16:27:46 +0530

    first commit

---
+++ styles.cs
@@ -0,0 +1,56 @@
+*{margin:0
+padding:0
+box-sizing;borderbox;
+front-family;"poppins",sans-serif;
+}
+
+{
+    container
+    background color:rosybrown
+    height : 600%
+    width: 100%
+    position:absulute
+
+    box
+}
+
+{box
+background-color:white;
+height:380%
+width:450%
+margin:-left:420
+margin-top:100
+padding:40
+}
+
+
+{img
+margin-bottom:99%
+}
+
+
+{h1
+margin-bottom:10%
+}
+
+
+{input
+margin-bottom:10%
+width:100%
+height:35%
+border:none;
+border-bottom:1%blacksolid;
+}
+
+
+
+{
+    p
+    margin-top:20%
+    height:35%
+    width:120%
+    margin-left:65%
+    background-color:bluevoielet
+    border-none;
+    color-white;
+}
